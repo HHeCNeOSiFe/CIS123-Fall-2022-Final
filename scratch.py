d2747858324ac1d3872e1f4746b5b0c70ec9eea6
HHeCNeOSiFe <94591275+HHeCNeOSiFe@users.noreply.github.com>
2022-12-02 14:07:06 -0500

    Delete scratch.py

--- scratch.py
+++
@@ -1,9 +0,0 @@
-from textwrap import dedent
-
-text = '''As you look around you notice that on the cryo-bed next to the one you were just in there is a light flashing red.
-          You follow the prompts on the cryo-bed interface and see that the power source has become disconnected.
-          Hopefully that doesn't mean 'damaged' as you're pretty sure you're running low on backups.
-          You should go check the connection. If you remember correctly, it is located in the floor of the room above.'''
-
-print(text)
-print(dedent(text))
